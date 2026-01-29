055ab8de81e10c86c1686f974aea036d174ffcaa
A2kmoise <abayomoise950@gmail.com>
2026-01-29 06:24:12 +0200

    -{ * * }- owner controller

--- src/main/java/org/rent/rentify/controller/OwnerController.java
+++ src/main/java/org/rent/rentify/controller/OwnerController.java
@@ -16,7 +16,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/owner")
+@RequestMapping("/api/v1/owner")
 public class OwnerController {
 
     private final OwnerService ownerService;
