8896612f07180d0d42760b1ae60cb90ad0fefd7f
A2kmoise <abayomoise950@gmail.com>
2026-01-31 14:50:40 +0200

    -{ * * }- AdminService feature

--- src/main/java/org/rent/rentify/service/AdminService.java
+++ src/main/java/org/rent/rentify/service/AdminService.java
@@ -1,8 +1,13 @@
 package org.rent.rentify.service;
 
+import org.rent.rentify.model.User;
+import org.rent.rentify.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AdminService {
 
+    public User totalUsers(){
+        return();
+    }
 }
